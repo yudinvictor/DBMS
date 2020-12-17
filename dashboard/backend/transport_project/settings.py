b0d6e00eb1475980b424ce1df478c48eac31e261
ilya <ilya-z2204@mail.ru>
2020-12-18 00:53:53 +0300

    add cors headers

--- dashboard/backend/transport_project/settings.py
+++ dashboard/backend/transport_project/settings.py
@@ -39,11 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'transport.apps.TransportConfig'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
